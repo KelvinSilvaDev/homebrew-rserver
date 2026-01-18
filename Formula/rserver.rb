7918e4e2ab59b43aa8e1d654edaa3b66eecc7284
Kelvin Oliveira <kelvinsilvadev@gmail.com>
2026-01-18 13:20:52 -0300

    Update rserver to v0.0.2

--- Formula/rserver.rb
+++ Formula/rserver.rb
@@ -4,21 +4,16 @@
 class Rserver < Formula
   desc "Remote Server Control - CLI multiplataforma para gerenciar serviços remotos"
   homepage "https://github.com/KelvinSilvaDev/rserver"
-  url "https://github.com/KelvinSilvaDev/rserver/archive/0.0.1.tar.gz"
-  sha256 "45d92546765d937b9cdee55408fd765bc2831a0ca19fe808cdcfd3eac5017d80"
+  url "https://github.com/KelvinSilvaDev/rserver/archive/v0.0.2.tar.gz"
+  sha256 "15912b4f4acecd0b6cd2bab29bb7a98449f6c2a1965016185e2b6db3e92c8e3e"
   license "Apache-2.0"
   head "https://github.com/KelvinSilvaDev/rserver.git", branch: "main"
 
   depends_on "python@3.9"
 
   def install
-    # O release 0.0.1 tem setup.py quebrado: procura rsctl mas estrutura é src/cli, src/core, etc
+    # A estrutura do release 0.0.2 está correta, não precisa de correções
     cd "cli" do
-      # Corrigir entry point
-      inreplace "setup.py", 'rserver=rsctl.cli.commands:main', 'rserver=cli.cli.commands:main'
-      # Remover completamente o bloco package_data (causa erro "package directory 'src/rsctl' does not exist")
-      # Substituir todo o bloco package_data por um dicionário vazio
-      inreplace "setup.py", /package_data=\{[^}]*\},/, 'package_data={},'
       system "python3", "-m", "pip", "install", "--prefix=#{prefix}", "."
     end
   end
